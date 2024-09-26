9fd756502598abf9724d4e736c6b68996f8b136b
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-09-26 17:54:47 -0300

    :sparkles: feat: Adicionar  um novo componente de switch para permitir que o usuário ative ou desative as notificações do aplicativo.

--- src/app/(pomopro)/components/header/dropdown-options.tsx
+++ src/app/(pomopro)/components/header/dropdown-options.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -10,34 +12,78 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Sun, User } from "lucide-react";
+import { Sun, User, Bell, BellOff } from "lucide-react";
 import Image from "next/image";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "@/app/lib/firebase/config";
 import { ButtonLogout } from "./button-logout";
 import { DropdownTheme } from "./dropdown-theme";
+import { Switch } from "@/components/ui/switch";
+import { toast } from "@/hooks/use-toast";
+import { useAuth } from "@/hooks/use-auth";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function DropdownOptions() {
-  const [user] = useAuthState(auth);
+  const { user, loading } = useAuth();
+  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
+
+  useEffect(() => {
+    if ("Notification" in window) {
+      setNotificationsEnabled(Notification.permission === "granted");
+    }
+  }, []);
+
+  const toggleNotifications = async () => {
+    if ("Notification" in window) {
+      if (notificationsEnabled) {
+        setNotificationsEnabled(false);
+        toast({
+          title: "Notificações desativadas",
+          description:
+            "Você não receberá mais notificações do nosso aplicativo.",
+        });
+      } else {
+        const permission = await Notification.requestPermission();
+        setNotificationsEnabled(permission === "granted");
+        if (permission === "granted") {
+          toast({
+            title: "Notificações ativadas",
+            description:
+              "Você receberá notificações importantes do nosso aplicativo.",
+          });
+        }
+      }
+    }
+  };
+
+  if (loading) return <Skeleton className="w-10 h-10 rounded-full" />;
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="w-10 h-10 rounded-full p-0">
-          {user?.photoURL && (
+          {user?.photoURL ? (
             <Image
               src={user.photoURL}
-              alt="Profile"
+              alt="Foto de perfil"
               width={40}
               height={40}
               className="w-full h-full rounded-full"
             />
+          ) : (
+            <User className="w-6 h-6" />
           )}
-          <User className="w-6 h-6" />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuLabel>Configurações</DropdownMenuLabel>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel>
+          <div className="flex flex-col space-y-2 p-1">
+            <p className="text-sm font-medium leading-none">
+              {`Olá ${user?.displayName} 👋` || "Olá Usuário 👋"}
+            </p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {user?.email || "usuario@exemplo.com"}
+            </p>
+          </div>
+        </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuSub>
           <DropdownMenuSubTrigger>
@@ -46,6 +92,19 @@ export function DropdownOptions() {
           </DropdownMenuSubTrigger>
           <DropdownTheme />
         </DropdownMenuSub>
+        <DropdownMenuItem onClick={toggleNotifications}>
+          {notificationsEnabled ? (
+            <Bell className="mr-2 h-4 w-4" />
+          ) : (
+            <BellOff className="mr-2 h-4 w-4" />
+          )}
+          <span>Notificações</span>
+          <Switch
+            checked={notificationsEnabled}
+            onCheckedChange={toggleNotifications}
+            className="ml-auto"
+          />
+        </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
           <ButtonLogout />
--- src/app/(pomopro)/components/header/header.tsx
+++ src/app/(pomopro)/components/header/header.tsx
@@ -1,38 +1,25 @@
-"use client";
+
 import React from "react";
 import { DropdownOptions } from "./dropdown-options";
-import { useAuth } from "@/hooks/use-auth";
 import Image from "next/image";
 import logo from "@/app/icon.png";
+
 export function Header() {
-  const { user, loading } = useAuth();
-
-  if (loading)
-    return (
-      <header className="p-4 flex justify-between items-center sticky top-0 z-50 border-b">
-        <div className="flex items-center justify-center">
-          <h1 className="text-2xl font-semibold">Carregando...</h1>
-        </div>
-      </header>
-    );
-
   return (
-    <header className="p-4 flex justify-between items-center sticky top-0 z-50 border-b bg-background">
-      <div className="flex items-center justify-center">
-        {user?.displayName && (
-          <h1 className="text-xl font-semibold">Olá, {user.displayName}! 👋</h1>
-        )}
-        {!user?.displayName && (
-          <Image
-            src={logo}
-            alt="PomoPro"
-            width={50}
-            height={50}
-            className="rounded-full"
-          />
-        )}
+    <header className="h-16 px-4 flex justify-between items-center sticky top-0 z-50 border-b bg-background">
+      <div className="flex items-center gap-2">
+        <Image
+          src={logo}
+          alt="PomoPro"
+          width={30}
+          height={30}
+          className="rounded-full"
+        />
+        <h1 className="text-2xl font-bold">PomoPro</h1>
       </div>
-      <DropdownOptions />
+      <div className="flex items-center gap-2">
+        <DropdownOptions />
+      </div>
     </header>
   );
 }
