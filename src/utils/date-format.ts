df09037376f3a539b7746d7823976d2b1988d3c0
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-09-27 00:13:45 -0300

    :sparkles: feat: Adicionar utilitários de formatação de data e obtenção de valores
    Adiciona os utilitários de formatação de data e obtenção de valores para serem utilizados em outras partes do código.
    
    - Adiciona o utilitário timeInMinutes para calcular o tempo em minutos.
    - Adiciona o utilitário formatDate para formatar a data no formato "dd/mm/yyyy hh:mm".
    - Adiciona o utilitário formatTime para formatar o tempo no formato "mm:ss".
    - Adiciona o utilitário getStatusIcon para obter o ícone correspondente ao status.
    - Adiciona o utilitário getStatusColor para obter a cor correspondente ao status.

---
+++ src/utils/date-format.ts
@@ -0,0 +1,20 @@
+export const timeInMinutes = (time: number) => Math.floor(time / 60);
+
+
+export const formatDate = (date: string) => {
+  return new Date(date).toLocaleString("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+};
+
+export const formatTime = (time: number): string => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  };
