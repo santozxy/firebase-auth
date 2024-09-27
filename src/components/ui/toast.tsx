6cd3cfc5d946349ab36e6a1d2cba49ca8ffe3cd6
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-09-27 00:16:25 -0300

    :sparkles: feat: Adicionar estilos de sucesso ao componente de toast

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -32,7 +32,7 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
-          success: "success group border-success bg-success text-success-foreground",
+          success: "bg-green-400 group border-green-400  text-white",
       },
     },
     defaultVariants: {
