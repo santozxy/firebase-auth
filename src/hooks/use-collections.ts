a3d91bee4aa66d37a1258d5879c0fca6600f9691
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-09-27 00:17:23 -0300

    :sparkles: feat: Criação de hooks para obter tanto os dados do usuário como tambem os dados das collections do firebase

--- src/hooks/use-collections.ts
+++ src/hooks/use-collections.ts
@@ -1,29 +1,24 @@
 import { useState, useEffect } from "react";
-import { User } from "firebase/auth";
 import { auth, db } from "@/app/lib/firebase/config";
 import { collection, CollectionReference } from "firebase/firestore";
 
-export function useAuth() {
-  const [user, setUser] = useState<User | null>(null);
-  const [uuid, setUuid] = useState<string | null>(null);
+export function useCollection() {
   const [activitiesCollection, setActivitiesCollection] =
     useState<CollectionReference | null>(null);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
-      setUser(user);
       if (user) {
-        setUuid(user.uid);
         setActivitiesCollection(
           collection(db, "users", user.uid, "activities")
         );
       }
-      setLoading(false);
     });
 
     return () => unsubscribe();
   }, []);
 
-  return { user, loading, uuid, activitiesCollection };
+  return { activitiesCollection };
 }
+
+
