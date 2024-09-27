df96ec4f6594655ff80378fa2ec097c4ee50d4c1
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-09-27 00:10:47 -0300

    :truck: move: Atualizando imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "./providers/theme-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
 import { Plus_Jakarta_Sans } from "next/font/google";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
