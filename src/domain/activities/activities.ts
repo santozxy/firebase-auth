80877a552e8a9f2936787e0f656426db7aabb948
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-10-02 17:20:49 -0300

    :sparkles: feat: Criando pasta de activities em domain e criando suas requisições

--- src/domain/activities/activities.ts
+++ src/domain/activities/activities.ts
@@ -1,11 +1,14 @@
 // In @/app/lib/firebase/activities.ts
 
+import { db } from "@/app/lib/firebase/config";
+import { toast } from "@/hooks/use-toast";
 import {
   query,
   where,
   getDocs,
   deleteDoc,
   CollectionReference,
+  doc,
 } from "firebase/firestore";
 
 export async function deleteInProgressAndPendingActivities(
@@ -23,5 +26,24 @@ export async function deleteInProgressAndPendingActivities(
     console.log("All in-progress and pending activities deleted successfully");
   } catch (error) {
     console.error("Error deleting activities:", error);
+  }
+}
+
+export async function deleteAcitivity(uuid: string, id: string) {
+  try {
+    await deleteDoc(doc(db, "users", uuid, "activities", id));
+    toast({
+      itemID: "activity-delete-success",
+      title: "Sucesso",
+      description: "Atividade excluída com sucesso.",
+    });
+  } catch (error) {
+    toast({
+      itemID: "activity-delete-error",
+      title: "Erro",
+      description: "Falha ao excluir atividade. Por favor, tente novamente.",
+      variant: "destructive",
+    });
+    console.error("Error deleting activity:", error);
   }
 }
