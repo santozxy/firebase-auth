49fbf02e255dfd77cbfdaa385a8c69db01b76a80
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-10-02 17:27:11 -0300

    :sparkles: feat: Adicionar cache de atividades no histórico
    Adiciona a funcionalidade de cache para as atividades do histórico, utilizando a função getHistoryActivityCached do domínio de histórico. Também adiciona a verificação do usuário atual antes de buscar as atividades, utilizando a função getCurrentUser do Firebase. Caso não haja um usuário logado, retorna nulo. Atualiza o componente History para exibir as atividades do cache, caso existam.

--- src/app/(pomopro)/history/page.tsx
+++ src/app/(pomopro)/history/page.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { ActivityHistory } from "./components/activity-history/activity-history";
+import { getHistoryActivityCached } from "@/domain/history/history";
+import { getCurrentUser } from "@/app/lib/firebase/admin";
 
-export default function History() {
-  return <ActivityHistory />;
+export default async function History() {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) return null;
+  const historyActivity = await getHistoryActivityCached(currentUser.uid);
+  return (
+    <main>
+      <ActivityHistory activities={historyActivity} />
+    </main>
+  );
 }
